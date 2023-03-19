a7fbaa6a3c1cf8fdfa3e97c1a2108c8b2ca93f39
red <red.avtovo@gmail.com>
2023-03-19 10:23:49 +0100

    updated registry prefix

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,4 +1,4 @@
 const val GROUP_ID = "com.j0rsa.labot"
 const val VERSION = "0.0.1"
-const val DOCKER_REGISTRY = "ghcr.io"
+const val DOCKER_REGISTRY = "ghcr.io/j0rsa"
 const val MAIN_CLASS = "com.j0rsa.labot.Main"
