a7d199da07acbe6a915a493018be9da7eb455c6b
red <red.avtovo@gmail.com>
2023-03-19 09:42:59 +0100

    cleanup ci

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,4 +1,4 @@
 const val GROUP_ID = "com.j0rsa.labot"
 const val VERSION = "0.0.1"
-const val DOCKER_REGISTRY = "docker.io"
+const val DOCKER_REGISTRY = "ghcr.io"
 const val MAIN_CLASS = "com.j0rsa.labot.Main"
