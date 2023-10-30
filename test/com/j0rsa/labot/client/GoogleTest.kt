203e2d9cdfcec39ea59e5fd3d0ac6266b395dc0a
red <red.avtovo@gmail.com>
2023-10-30 14:02:57 +0100

    update Google test

--- test/com/j0rsa/labot/client/GoogleTest.kt
+++ test/com/j0rsa/labot/client/GoogleTest.kt
@@ -2,13 +2,12 @@ package com.j0rsa.labot.client
 
 import com.j0rsa.labot.client.support.Google
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.collections.shouldBeOneOf
-import io.kotest.matchers.shouldBe
+import io.kotest.matchers.collections.shouldBeIn
 
 class GoogleTest : StringSpec({
 
     "translate" {
-        Google.translateEnToRu("Some example") shouldBeOneOf listOf(
+        Google.translateEnToRu("Some example") shouldBeIn listOf(
             "Какой-то пример",
             "Некоторый пример",
         )
